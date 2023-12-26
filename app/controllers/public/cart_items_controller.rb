bda28a56009a78847a63cca1c47d511a33866344
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2023-12-26 13:18:39 +0000

    [new]cart_item

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,11 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
   def index
+    @cart_items = CartItem.all
   end
 
   def update
+    @cart_item.update(cart_item_params)
   end
 
   def destroy
@@ -13,5 +15,10 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
