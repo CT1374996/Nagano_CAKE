d290e6ab5ece125f841409d01257eb845a8f8c0e
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-08 07:52:43 +0000

    [update]cart_item create

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -23,13 +23,15 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
-    @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer.id
-    if @cart_item.save
-      redirect_to cart_items_path(current_customer)
+    cart_item = CartItem.find_by(item_id: params[:cart_item][:item_id])
+    if cart_item.nil?
+      @cart_item = CartItem.new(cart_item_params)
+      @cart_item.customer_id = current_customer.id
+      @cart_item.save
     else
-      render :show
+      cart_item.update(amount: cart_item.amount + params[:cart_item][:amount].to_i)
     end
+      redirect_to cart_items_path
   end
 
   private
