4dd7d643058aa81f383c1151315253c9ead056c1
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-11 10:48:21 +0000

    NameError

--- db/migrate/20240111043951_add_postal_code_to_address.rb
+++
@@ -1,5 +0,0 @@
-class AddPostalCodeToAddress < ActiveRecord::Migration[6.1]
-  def change
-    add_column :addresses, :postal_code, :string
-  end
-end
--- db/migrate/20240111044713_remove_address_from_address.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAddressFromAddress < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :addresses, :address, :string
-  end
-end
