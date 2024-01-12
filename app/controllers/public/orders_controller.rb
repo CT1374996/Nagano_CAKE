80c10e9e6c38e1a9c4c767c561d0a6a64ffa9e81
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-12 06:40:50 +0000

    [update]order

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,18 +5,34 @@ class Public::OrdersController < ApplicationController
   end
 
   def confirm
-    @order = current_customer
+    @order = Order.new(order_params)
+    @order.postal_code = current_customer.postal_code
+    @order.address = current_customer.address
+    @order.name = current_customer.last_name + first_name
+    @total = 0
   end
 
   def complete
+    @customer = current_customer
+    current_customer.cart_items.destroy_all
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :monetary_amount)
   end
 end
