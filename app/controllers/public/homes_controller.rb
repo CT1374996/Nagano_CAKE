4a4cce2d1d8b21b0a97ce7dc76f475cbf2b1ee29
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-18 12:14:50 +0000

    [update]pub,top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.order(created_at: :desc).first(4)
+    #@items = Item.first(4)
+    #@items = Item.last(4)
   end
 
   def about
