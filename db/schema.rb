7fae5277af4762838d396889db02d150906f07d8
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-20 06:16:38 +0000

    [update]layouts

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_11_044713) do
-
-  create_table "addresses", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.string "postal_code"
-  end
+ActiveRecord::Schema.define(version: 2023_11_21_093335) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
