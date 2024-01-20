0d3ea8eea0164e31313f1976c893ae43f2ba4ae8
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-01-20 07:28:00 +0000

    [test]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -8,7 +8,7 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resoureces)
-    about_path
+    root_path
   end
   # before_action :configure_sign_in_params, only: [:create]
 
