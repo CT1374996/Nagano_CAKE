acb7bc9c415e03a02aa8ecdd9831e2bf4b065b1d
CT1374996 <ec2-user@ip-172-31-29-162.ap-northeast-1.compute.internal>
2024-07-29 08:04:45 +0000

    [update]genre

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -19,7 +19,7 @@ class Admin::ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-    @genre = Genre.find(params[:genre_id])
+    @genre = Genre.find(params[:id])
   end
 
   def edit
